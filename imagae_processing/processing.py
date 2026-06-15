cb4e36b3f9f6ee40e67478caec3c41efa15a70ec
ABHI9029 <abhishek.s@themailwhale.in>
2026-06-15 15:31:53 +0530

    updated

--- imagae_processing/processing.py
+++ imagae_processing/processing.py
@@ -1,13 +1,18 @@
 from skimage.color import rgb2gray
 from skimage import io 
+from skimage import transform
 import matplotlib.pyplot as plt
 
-
+#image read
 image = io.imread("image.jpg")
 
 #convert it into grey scale
 grey = rgb2gray(image)
 plt.imshow(grey,cmap="grey")
+
+resized_image = transform.resize(image, (100, 100))
+io.imshow(resized_image)
+
 
 plt.savefig("output.png")
 plt.title("Original image")
